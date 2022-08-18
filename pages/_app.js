4d6d28a719ea17f0f6b3bfc161ae493f2425f959
Jhonattan Tunales <70348160+RostCode78@users.noreply.github.com>
2022-08-18 18:53:55 -0500

    OpenGraph

--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,14 @@ function Rost({ Component, pageProps }) {
             <Head>
                 <title>Rost78 / Jhonattan Tunales</title>
                 <link rel="icon" type="image/x-icon" href="/assets/favicon.png"/>
+                <meta property="og:title" content="Rost78 / Jhonattan Tunales" />
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content="https://www.rost78.com/" />
+                <meta property="og:image" content="https://i.imgur.com/RE5syC5.png" />
+                <meta property="og:site_name" content="rost78" />
+                <meta property="og:locale" content="es_MX" />
+                <meta property="og:locale:alternate" content="es_MX" />
+                <meta property="og:description" content="Mi nombre es Jhonattan tunales. Me gusta hacer diseño web y luego, llevarlo a codigo, en mis tiempos libres, me gusta jugar Halo y leer a Tolkien." />
             </Head>
             <Component
                 {...pageProps}
