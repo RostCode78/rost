e0ca6c35cc9df7173dd6448c53d93c5768d84b68
JhonattanTunales <zeroxcode78@gmail.com>
2024-11-21 09:30:44 -0600

    Feat: Contacto

--- components/Index/About/About.js
+++ components/Index/About/About.js
@@ -6,24 +6,28 @@ const About = () => {
         <div className="container-about">
             <div className="content">
                 <div className="left">
-                    <p>Mi nombre es <span className="color-blue">Jhonattan tunales</span>. <br/>Me gusta hacer <span className="color-yellow">diseño web</span> y  luego<br/>llevarlo a <span className="color-yellow">código</span>, en mis tiempos libres<br/>me gusta jugar <span className="color-blue">Halo</span> y leer a <span className="color-blue">Tolkien</span>.</p>
+                    <p>Mi nombre es <span className="color-blue">Jhonattan Tunales</span>. <br/>Me gusta hacer <span className="color-yellow">diseño web</span> y  luego<br/>llevarlo a <span className="color-yellow">código</span>, en mis tiempos libres<br/>me gusta jugar <span className="color-blue">Halo</span> y leer a <span className="color-blue">Tolkien</span>.</p>
                 </div>
                 <div className="right">
-                    <p>¿Quieres saber más sobre mi o contactarme?</p>
-                    <div className="correo">
-                        <p>Enviame un correo:</p>
-                        <span>zeroxcode78@gmail.com</span>
+                    <p>¿Quieres ponerte en contacto conmigo?</p>
+                    <div className="boton">
+                        <p>Envíame un correo:</p>
+                        <a href="mailto:ros.tunales@gmail.com" target="_blank">
+                            <button>
+                                <p>ros.tunales@gmail.com</p>
+                            </button>
+                        </a>
                     </div>
                     <div className="boton">
-                        <p>Revisa mi Instagram:</p>
-                        <a href="https://www.instagram.com/dev_rost78/" target="_blank">
+                        <p>Envíame un mensaje:</p>
+                        <a href="https://wa.me/528118957772" target="_blank">
                             <button>
                                 <Image
-                                    src="/icons/instagram.svg"
+                                    src="/icons/WhatsApp.svg"
                                     width={18}
                                     height={18}
                                 />
-                                <p>Dev_Rost78</p>
+                                <p>Whatsapp</p>
                             </button>
                         </a>
                     </div>
